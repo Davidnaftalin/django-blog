3d47ee640adf64cf42d7231525c358db0408e0ca
davidnaftalin <davidnaftalin@gmail.com>
2014-05-10 11:54:59 +0100

    Added Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=99, blank=True, null=True)
+    content = model.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, unique=True)
+
+    def __unicode__(self):
+        return '{} - {} - {}'.format(self.title, self.author, self.created)
